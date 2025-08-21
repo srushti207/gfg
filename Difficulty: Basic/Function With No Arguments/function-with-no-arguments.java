5c10eac73779313b7a62847eac5bd782f5a6d15f
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-21 15:16:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With No Arguments/function-with-no-arguments.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+// Write the helloFunction below. It should have one statement
+// System.out.println("Hello")
+
+class Solution {
+    //  code here
+    static void helloFunction(){
+        System.out.println("Hello");
+    }
+}
