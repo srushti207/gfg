36e661042c12ef73c1b80737d1d1db1910dac53f
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-21 16:23:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<11;i++){
+            System.out.print(n*i + " ");
+        }
+    }
+}
