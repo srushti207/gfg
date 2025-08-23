2d95f6dc5d1fe274b89ea467ace413c3135aea1a
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-23 15:40:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/For Loop - 2/for-loop-2.java
+++ Difficulty: Basic/For Loop - 2/for-loop-2.java
@@ -1,8 +1,8 @@
 class Solution {
-    public static void utility(String s) {
+    public void printEvenIndices(String s) {
         // code here
-        int n = s.length();
-        for(int i=0;i<n;i++){
+        String ans = "";
+        for(int i=0;i<=s.length();i++){
             if(i%2 == 0){
                 System.out.print(s.charAt(i));
             }
