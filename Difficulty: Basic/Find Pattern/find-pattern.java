3a0c7a83810fa6868e7dc78ef9c5691b23e5c807
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-23 15:20:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find Pattern/find-pattern.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    // function should find if the string p is
+    // in the string s. Return the starting index
+    // of the string p in s. Return -1 if the
+    // string doesnot exists in s.
+    public static int findPattern(String s, String p) {
+        // code here
+        
+        return s.indexOf(p);
+        
+        
+    }
+}
