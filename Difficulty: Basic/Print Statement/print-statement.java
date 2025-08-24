67168ae4510c8ca10c4b7ff8295f7ff41887e964
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-24 15:13:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Statement/print-statement.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        // Print S and move the cursor to a new line
+        System.out.print(s);
+    }
+}
