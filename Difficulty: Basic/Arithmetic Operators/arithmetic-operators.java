73e779370f0c150f1f8c479fc8bd6ec8f08960d7
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-24 15:14:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
@@ -1,12 +1,19 @@
+// User function Template for Java
 class Solution {
-    public int[] computeOperations(int x, int y) {
-        // code here
-        int add = x+y;
-        int sub = x-y;
-        int mul = x*y;
-        int div = x/y;
-        int mod = x%y;
-        int[] arr = {add,sub,mul,div,mod};
-        return arr;
+
+    public static void operations(int x, int y) {
+        // Perform addition x+y below
+        int p = x+y;
+            // Perform subtraction x-y below
+            int q =x-y;
+                // Perform multiplication x*y below
+            int r =x*y;
+                // Perform division x/y below
+            int s =x/y;
+                // Perform modulo operation x%y below
+            int t =x%y;
+
+                // The below code prints the output. Don't change it!
+            System.out.println(p + " " + q + " " + r + " " + s + " " + t);
     }
 }
