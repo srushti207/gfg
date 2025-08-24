f5edf284c5b52102f8b9c7055e09f59394c09c73
srushti207 <139860515+srushti207@users.noreply.github.com>
2025-08-24 15:17:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print With Separator/print-with-separator.java
+++ Difficulty: Basic/Print With Separator/print-with-separator.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 public class Solution {
-    public static void utility(Scanner scn) {
-        String a = scn.nextLine();
-        String b = scn.nextLine();
-        String separator = scn.nextLine();
-        // print with seperator
-        System.out.println(a+""+separator+""+b);
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String sep = sc.nextLine();
+        // Print in the required format and a new line
+        System.out.println(a+""+sep+""+b);
     }
 }
